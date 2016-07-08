57676551b8befd41726334a71621cf8cf7b6c103
finiteprods <kwok.doc@gmail.com>
2016-07-08 11:50:59 +0100

    added another query response

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -8,6 +8,9 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
+        if (query.toLowerCase().contains("kwok cheung")) {
+            return "Kwok-Ho Cheung is a DPhil student in Computer Science, at the University of Oxford.";
+        }
         return "";
     }
 }
